442653870c6273b98ccb3403bacb0a69c7afd05f
denn nevera (mbp17) <denn.nevera@gmail.com>
2019-01-30 14:43:41 +0300

    update api docs

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,11 +1,13 @@
 var searchData=
 [
   ['data',['Data',['../classed25519_1_1_data.html',1,'ed25519::Data&lt; N &gt;'],['../classed25519_1_1_data.html#a2c637587095d6527cd4136926fb8b452',1,'ed25519::Data::Data()']]],
+  ['data_3c_20size_3a_3adigest_20_3e',['Data&lt; size::digest &gt;',['../classed25519_1_1_data.html',1,'ed25519']]],
   ['data_3c_20size_3a_3aprivate_5fkey_20_3e',['Data&lt; size::private_key &gt;',['../classed25519_1_1_data.html',1,'ed25519']]],
   ['data_3c_20size_3a_3apublic_5fkey_20_3e',['Data&lt; size::public_key &gt;',['../classed25519_1_1_data.html',1,'ed25519']]],
   ['data_3c_20size_3a_3aseed_20_3e',['Data&lt; size::seed &gt;',['../classed25519_1_1_data.html',1,'ed25519']]],
+  ['data_3c_20size_3a_3asignature_20_3e',['Data&lt; size::signature &gt;',['../classed25519_1_1_data.html',1,'ed25519']]],
   ['decode',['decode',['../classed25519_1_1_base58.html#a3cb74be32923dcfb03a24b65015bee84',1,'ed25519::Base58::decode()'],['../classed25519_1_1_data.html#a281d932d3c3fe7fd40ce86ea7eff559b',1,'ed25519::Data::decode()'],['../namespaceed25519_1_1base58.html#ac8132589f5098b8de74d2dfb72f7bf65',1,'ed25519::base58::decode(const std::string &amp;str, std::vector&lt; unsigned char &gt; &amp;data)'],['../namespaceed25519_1_1base58.html#a97f34fe5ffdd0da677a4cef6be3a5a60',1,'ed25519::base58::decode(const std::string &amp;base58, std::array&lt; unsigned char, N &gt; &amp;data, const ErrorHandler &amp;error=default_error_handler)']]],
   ['default_5ferror_5fhandler',['default_error_handler',['../namespaceed25519.html#a7c7bb6ed17541162959c33ed3e3b15fb',1,'ed25519']]],
-  ['digest',['digest',['../namespaceed25519_1_1size.html#ab3443d829236034a3824204b295de4d0',1,'ed25519::size']]],
+  ['digest',['Digest',['../structed25519_1_1_digest.html',1,'ed25519::Digest'],['../namespaceed25519_1_1size.html#ab3443d829236034a3824204b295de4d0',1,'ed25519::size::digest()']]],
   ['double_5fhash',['double_hash',['../namespaceed25519_1_1size.html#ac853f864bb12792f88647a998c3c030f',1,'ed25519::size']]]
 ];
